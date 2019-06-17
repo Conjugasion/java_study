98421af754cca084ef6fc0a3e1245f7f944a7dd8
pomhunter <tdf516020608@163.com>
2019-06-17 16:44:09 +0800

    cisco

--- main/algorithm/sort/MergeSort.java
+++ main/algorithm/sort/MergeSort.java
@@ -23,51 +23,56 @@ public class MergeSort {
         for (int i = 0; i < s.length; i++) {
             array[i] = Integer.valueOf(s[i]);
         }
-        int[] result = new int[array.length];
-        sort(array, 0, array.length-1, result);
-        System.out.println(Arrays.toString(result));
+        sort(array, 0, array.length-1);
+        System.out.println(Arrays.toString(array));
     }
 
     // 排序
-    static void sort(int[] array, int left, int right, int[] result){
-        if (left < right) {
-            int mid = (left + right) / 2;
-            sort(array, left, mid, result);    //左边有序
-            sort(array, mid + 1, right, result); //右边有序
-            merge(array, left, right, result); //再将二个有序数列合并
+    static void sort(int[] array, int left, int right){
+        if (left < right){
+            int mid = (left + right)/2;
+            sort(array, left, mid);          // 对左半边排序
+            sort(array, mid+1, right);  // 对右半边排序
+            merge(array, left, mid, right);
         }
     }
 
     // 合并
-    static void merge(int[] array, int left, int right, int[] result){
-        if (left < right) return;
-
-        int i = left;
-        int mid = (left+right)/2;
-        int j = mid + 1;
-        int n = 0;
-
-        while (i <= mid && j <= right){
-            if (array[i] <= array[j]){
-                result[n] = array[i];
+    static void merge(int[] array, int left, int mid, int right){
+        // 拆成两个数组
+        int[] l = new int[mid-left+1];
+        int[] r = new int[right-mid];
+        for (int i = left; i <= mid; i++) {
+            l[i-left] = array[i];
+        }
+        for (int i = mid+1; i <= right; i++) {
+            r[i-mid-1] = array[i];
+        }
+        // 两个数组比较大小
+        int i = 0;
+        int j = 0;
+        int n = left;
+        while (i <= mid-left && j <= right-mid-1){
+            if (l[i] <= r[j]) {
+                array[n] = l[i];
                 n++;
                 i++;
-            }
-            else {
-                result[n] = array[j];
+            } else {
+                array[n] = r[j];
                 n++;
                 j++;
             }
         }
-        if (i <= mid){
-            for (int k = i; k <= mid ; k++) {
-                result[n] = array[i];
+
+        if (i <= mid-left){
+            for (int k = i; k <= mid-left; k++) {
+                array[n] = l[k];
                 n++;
             }
         }
-        if (j <= right){
-            for (int k = j; k <= right ; k++) {
-                result[n] = array[j];
+        if (j <= right-mid-1){
+            for (int k = j; k <= right-mid-1; k++) {
+                array[n] = r[k];
                 n++;
             }
         }
