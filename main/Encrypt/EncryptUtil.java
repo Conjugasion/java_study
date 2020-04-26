b6215806200215eb4c918bcff8682a59bab1e8a1
tangdongfan <tangdongfan@jd.com>
2020-04-26 20:32:41 +0800

    RSA加密解密工具改进

--- main/Encrypt/EncryptUtil.java
+++ main/Encrypt/EncryptUtil.java
@@ -1,9 +1,15 @@
 package Encrypt;
 
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
 import javax.crypto.Cipher;
 import java.security.Key;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,42 +21,38 @@ import java.util.Map;
  */
 public class EncryptUtil {
 
-    public static final String PRIVATE_KEY = "privateKey";
-    public static final String PUBLIC_KEY = "publicKey";
+    public static final Key PUBLIC_KEY;
+    public static final Key PRIVATE_KEY;
 
     // 生成公钥和私钥
-    public static Map<String, Key> generatorKeys() {
+    static {
+        //KeyPairGenerator类用于生成公钥和私钥对，基于RSA算法生成对象
+        KeyPairGenerator keyPairGen = null;
         try {
-            Map<String,Key> keys = new HashMap<>();
-            //KeyPairGenerator类用于生成公钥和私钥对，基于RSA算法生成对象
-            KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
-            //初始化密钥对生成器，密钥大小为1024位
-            keyPairGen.initialize(1024);
-            //生成一个密钥对，保存在keyPair中
-            KeyPair keyPair = keyPairGen.generateKeyPair();
-            //得到私钥
-            Key privateKey = keyPair.getPrivate();
-            //得到公钥
-            Key publicKey = keyPair.getPublic();
-            keys.put(PRIVATE_KEY, privateKey);
-            keys.put(PUBLIC_KEY, publicKey);
-            return keys;
-        } catch (Exception e) {
+            keyPairGen = KeyPairGenerator.getInstance("RSA");
+        } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
-        return null;
+        //初始化密钥对生成器，密钥大小512以上
+        keyPairGen.initialize(512);
+        //生成一个密钥对，保存在keyPair中
+        KeyPair keyPair = keyPairGen.generateKeyPair();
+        //得到私钥
+        PRIVATE_KEY = keyPair.getPrivate();
+        //得到公钥
+        PUBLIC_KEY = keyPair.getPublic();
     }
 
     // 加密算法
-    public static byte[] encrypt(Key publicKey, String srcMsg) {
+    public static String encrypt(String srcMsg) {
         try {
             byte[] srcMsgByte = srcMsg.getBytes();
             //Cipher基于RSA进行加密
             Cipher cipher = Cipher.getInstance("RSA");
             //根据公钥，对Cipher对象进行初始化
-            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-            //加密
-            return cipher.doFinal(srcMsgByte);
+            cipher.init(Cipher.ENCRYPT_MODE, PUBLIC_KEY);
+            byte[] encryptMsgByte = cipher.doFinal(srcMsgByte);
+            return new BASE64Encoder().encode(encryptMsgByte).replaceAll("[\\s*\t\n\r]", "");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -58,18 +60,27 @@ public class EncryptUtil {
     }
 
     // 解密算法
-    public static String decrypt(Key privateKey, byte[] encryptMsgByte) {
+    public static String decrypt(String encryptMsg) {
         try {
+            byte[] encryptMsgByte = new BASE64Decoder().decodeBuffer(encryptMsg);
             //Cipher基于RSA进行解密
             Cipher cipher = Cipher.getInstance("RSA");
             //根据公钥，对Cipher对象进行初始化
-            cipher.init(Cipher.DECRYPT_MODE, privateKey);
-            //解密
+            cipher.init(Cipher.DECRYPT_MODE, PRIVATE_KEY);
             byte[] srcMsgByte = cipher.doFinal(encryptMsgByte);
             return new String(srcMsgByte);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static void main(String[] args) {
+        String srcMsg = "123asdas123asdddddddddddd";
+        String encrypt = encrypt(srcMsg);
+        String decrypt = decrypt(encrypt);
+        System.out.println("密文：" + encrypt);
+        System.out.println("密文长度：" + encrypt.length());
+        System.out.println("解密后：" + decrypt);
     }
 }
