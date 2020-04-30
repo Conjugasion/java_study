35c93450d5384ebaafdbf098eaa807a494498fa7
tangdongfan <tangdongfan@jd.com>
2020-04-30 11:47:14 +0800

    git reset Test

---
+++ main/Encrypt/gitTest.java
@@ -0,0 +1,11 @@
+package Encrypt;
+
+/**
+ * @author tangdongfan
+ * @date 2020/4/30 11:46
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("gitTest");
+    }
+}
