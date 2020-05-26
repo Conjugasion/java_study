e1a4571503e2c20f84319652f597c3b44c638481
tangdongfan <tangdongfan@jd.com>
2020-05-26 17:37:32 +0800

    fix

--- main/Json/LivingConfigList.java
+++ main/Json/LivingConfigList.java
@@ -5,7 +5,7 @@ import Util.DateTimeUtils;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.mysql.cj.xdevapi.JsonArray;
+
 
 import java.text.SimpleDateFormat;
 import java.util.*;
